479b0d4a90ccdbbcf346f35941a9b693b1a93b26
wuyinlei <wuyinlei19931118@hotmail.com>
2016-05-15 11:41:57 +0800

    加入存储过程知识，用java代码测试存储过程

---
+++ src/com/yinlei/JdbcUtils.java
@@ -0,0 +1,87 @@
+package com.yinlei;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+//通用的连接数据库的类
+public class JdbcUtils {
+
+	private static String driverClass = "";
+	private static String url = "";
+	private static String user = "";
+	private static String password = "";
+
+	// 在构造方法之前调用
+	static {
+		ResourceBundle rb = ResourceBundle.getBundle("dbcof");
+		driverClass = rb.getString("driverClass");
+		url = rb.getString("url");
+		user = rb.getString("user");
+		password = rb.getString("password");
+
+		try {
+			// 注册驱动
+			Class.forName(driverClass);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取连接
+	 * 
+	 * @return
+	 */
+	public static Connection getConnection() {
+		try {
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 释放资源
+	 * 
+	 * @param rs
+	 *            结果集
+	 * @param stmt
+	 *            查询
+	 * @param conn
+	 *            连接
+	 */
+	public static void release(ResultSet rs, Statement stmt, Connection conn) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/com/yinlei/ProcedureTest.java
@@ -0,0 +1,77 @@
+package com.yinlei;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+/**
+ * 演示JDBC调用存储过程
+ * 
+ * @author wuyin
+ *
+ */
+public class ProcedureTest {
+
+	/**
+	 * 测试不带返回值的存储过程
+	 * 
+	 * @throws SQLException
+	 */
+	public static void testPro() throws SQLException {
+		// 获得连接对象
+		Connection conn = JdbcUtils.getConnection();
+
+		// 创建一个执行存储过程的命令对象
+		CallableStatement cstms = conn.prepareCall("{call pro4(?,?)}");
+
+		// 指定"?"的值
+		cstms.setInt(1, 1);
+		// 指定第二个问号是输出参数
+		cstms.registerOutParameter(2, Types.VARCHAR);
+		// 执行存储过程
+		boolean i = cstms.execute();
+		// 获取返回值
+		String name = cstms.getString(2);
+		System.out.println("接收到的参数是：" + name);
+
+		System.out.println(i); // true
+		// 释放资源
+		JdbcUtils.release(null, cstms, conn);
+
+	}
+
+	/**
+	 * 测试带返回值的存储过程
+	 * 
+	 * @throws SQLException
+	 */
+	public static void testPro1() throws SQLException {
+		// 获得连接对象
+		Connection conn = JdbcUtils.getConnection();
+
+		// 创建一个执行存储过程的命令对象
+		CallableStatement cstms = conn.prepareCall("{call pro1(?)}");
+
+		// 指定"?"的值
+		cstms.setInt(1, 1);
+
+		// 执行存储过程
+		boolean i = cstms.execute();
+		System.out.println(i); // true
+		// 释放资源
+		JdbcUtils.release(null, cstms, conn);
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			testPro();
+			testPro1();
+			System.out.println("tiaodd");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
